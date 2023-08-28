2fcd0c6a2b4e1743c0330ff2f7e20db484705367
chandra-pro <goluchandra1812@gmail.com>
2023-08-28 20:24:43 +0530

    login screen

--- StackNavigator.js
+++ StackNavigator.js
@@ -3,6 +3,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
 import HomeScreen from "./screens/HomeScreen";
 import ProfileScreen from "./screens/ProfileScreen";
+import LoginScreen from "./screens/LoginScreen.js";
 import { Entypo } from "@expo/vector-icons";
 import { Ionicons } from "@expo/vector-icons";
 import { AntDesign } from "@expo/vector-icons";
@@ -67,7 +68,11 @@ function Navigation() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        {/* <Stack.Screen name="Login" component={LoginScreen} options={{headerShown:false}}/> */}
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ headerShown: false }}
+        />
         <Stack.Screen
           name="Main"
           component={MyTabs}
---
+++ screens/LoginScreen.js
@@ -0,0 +1,220 @@
+import { StyleSheet, Text, View, SafeAreaView, Pressable } from "react-native";
+import React, { useEffect } from "react";
+import { LinearGradient } from "expo-linear-gradient";
+import { Entypo } from "@expo/vector-icons";
+import { MaterialIcons } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
+
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useNavigation } from "@react-navigation/native";
+import * as WebBrowser from "expo-web-browser";
+import {
+  makeRedirectUri,
+  useAuthRequest,
+  ResponseType,
+} from "expo-auth-session";
+
+WebBrowser.maybeCompleteAuthSession();
+
+const discovery = {
+  authorizationEndpoint: "https://accounts.spotify.com/authorize",
+  tokenEndpoint: "https://accounts.spotify.com/api/token",
+};
+
+const LoginScreen = () => {
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    const checkTokenValidity = async () => {
+      const accessToken = await AsyncStorage.getItem("token");
+      const expirationDate = await AsyncStorage.getItem("expirationDate");
+      console.log("acess token", accessToken);
+      console.log("expiration date", expirationDate);
+
+      if (accessToken && expirationDate) {
+        const currentTime = Date.now();
+        if (currentTime < parseInt(expirationDate)) {
+          // here the token is still valid
+          navigation.replace("Main");
+        } else {
+          // token would be expired so we need to remove it from the async storage
+          AsyncStorage.removeItem("token");
+          AsyncStorage.removeItem("expirationDate");
+        }
+      }
+    };
+
+    checkTokenValidity();
+  }, []);
+
+  const [request, response, promptAsync] = useAuthRequest(
+    {
+      responseType: ResponseType.Token,
+      clientId: "0ed64c179d6342a4b1d3c1cf6039c1eb",
+      clientSecret: "15aa0975faf4479597fcc61eb2f10f17",
+      scopes: [
+        "user-read-email",
+        "user-library-read",
+        "user-read-recently-played",
+        "user-top-read",
+        "playlist-read-private",
+        "playlist-read-collaborative",
+        "playlist-modify-public",
+      ],
+      usePKCE: false,
+
+      redirectUri: "exp://127.0.0.1:19000/",
+    },
+    discovery
+  );
+
+  React.useEffect(() => {
+    if (response?.type === "success") {
+      const { access_token } = response.params;
+      console.log(response);
+      console.log("params", access_token);
+      // navigation.replace("Main");
+      const expirationDate = new Date(response.expiresIn).getTime();
+      AsyncStorage.setItem("token", access_token);
+      AsyncStorage.setItem("expirationDate", expirationDate.toString());
+      navigation.navigate("Main");
+    }
+  }, [response]);
+
+  return (
+    <LinearGradient colors={["#040306", "#131624"]} style={{ flex: 1 }}>
+      <SafeAreaView>
+        <View style={{ height: 80 }} />
+        <Entypo
+          style={{ textAlign: "center" }}
+          name="spotify"
+          size={80}
+          color="white"
+        />
+        <Text
+          style={{
+            color: "white",
+            fontSize: 40,
+            fontWeight: "bold",
+            textAlign: "center",
+            marginTop: 40,
+          }}
+        >
+          Millions of Songs Free on spotify!
+        </Text>
+
+        <View style={{ height: 80 }} />
+        <Pressable
+          onPress={() => promptAsync()}
+          style={{
+            backgroundColor: "#1DB954",
+            padding: 10,
+            marginLeft: "auto",
+            marginRight: "auto",
+            width: 300,
+            borderRadius: 25,
+            alignItems: "center",
+            justifyContent: "center",
+            marginVertical: 10,
+          }}
+        >
+          <Text>Sign In with spotify</Text>
+        </Pressable>
+
+        <Pressable
+          style={{
+            backgroundColor: "#131624",
+            padding: 10,
+            marginLeft: "auto",
+            marginRight: "auto",
+            width: 300,
+            borderRadius: 25,
+            alignItems: "center",
+            justifyContent: "center",
+            flexDirection: "row",
+            alignItems: "center",
+            marginVertical: 10,
+            borderColor: "#C0C0C0",
+            borderWidth: 0.8,
+          }}
+        >
+          <MaterialIcons name="phone-android" size={24} color="white" />
+          <Text
+            style={{
+              fontWeight: "500",
+              color: "white",
+              textAlign: "center",
+              flex: 1,
+            }}
+          >
+            Continue with phone number
+          </Text>
+        </Pressable>
+
+        <Pressable
+          style={{
+            backgroundColor: "#131624",
+            padding: 10,
+            marginLeft: "auto",
+            marginRight: "auto",
+            width: 300,
+            borderRadius: 25,
+            alignItems: "center",
+            justifyContent: "center",
+            flexDirection: "row",
+            alignItems: "center",
+            marginVertical: 10,
+            borderColor: "#C0C0C0",
+            borderWidth: 0.8,
+          }}
+        >
+          <AntDesign name="google" size={24} color="red" />
+          <Text
+            style={{
+              fontWeight: "500",
+              color: "white",
+              textAlign: "center",
+              flex: 1,
+            }}
+          >
+            Continue with Google
+          </Text>
+        </Pressable>
+
+        <Pressable
+          style={{
+            backgroundColor: "#131624",
+            padding: 10,
+            marginLeft: "auto",
+            marginRight: "auto",
+            width: 300,
+            borderRadius: 25,
+            alignItems: "center",
+            justifyContent: "center",
+            flexDirection: "row",
+            alignItems: "center",
+            marginVertical: 10,
+            borderColor: "#C0C0C0",
+            borderWidth: 0.8,
+          }}
+        >
+          <Entypo name="facebook" size={24} color="blue" />
+          <Text
+            style={{
+              fontWeight: "500",
+              color: "white",
+              textAlign: "center",
+              flex: 1,
+            }}
+          >
+            Sign In with facebook
+          </Text>
+        </Pressable>
+      </SafeAreaView>
+    </LinearGradient>
+  );
+};
+
+export default LoginScreen;
+
+const styles = StyleSheet.create({});
